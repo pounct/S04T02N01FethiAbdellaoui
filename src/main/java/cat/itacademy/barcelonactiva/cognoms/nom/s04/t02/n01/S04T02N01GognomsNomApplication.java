0e2f7d4af70443368bc5ba2c3a622ed3dcb8bfde
Pounct <53088375+pounct@users.noreply.github.com>
2023-09-14 02:17:05 +0200

    Update S04T02N01GognomsNomApplication.java

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GognomsNomApplication.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GognomsNomApplication.java
@@ -1,13 +1,21 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class S04T02N01GognomsNomApplication implements CommandLineRunner {
+public class ApiRestAmbSpringBootApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-		SpringApplication.run(S04T02N01GognomsNomApplication.class, args);
+		SpringApplication.run(ApiRestAmbSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		
 	}
 
 }
